1260a481de5f9260669ff386e367630a59e5c921
imuhikira <imuhikira@gmail.com>
2022-05-17 17:32:57 -0500

    prob solved

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,16 +1,16 @@
 
 
 function ageSort(users) {   //it's like to sey users.a(represents the index of the array age tapping into the object)
-  
-  return users.sort((a,b)=> {return a.age - b.age})
-  
+
+  return users.sort((a, b) => { return a.age - b.age })
+
 }
 
 function oddEvenSort(arr) {
-  
-  let result = arr.sort ((a,b)=> {
-    if (a % 2 === 1 && b % 2 ===0){ return -1}
-    if (b % 2 === 1 && a % 2 ===0){ return 1}
+
+  let result = arr.sort((a, b) => {
+    if (a % 2 === 1 && b % 2 === 0) { return -1 }
+    if (b % 2 === 1 && a % 2 === 0) { return 1 }
     return a - b
   })
 
@@ -18,7 +18,27 @@ function oddEvenSort(arr) {
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+
+  let len1 = s.length;
+  let len2 = t.length;
+  if(len1 !== len2){
+
+     return false;
+  }
+  let str1 = s.split('').sort().join('');
+
+  console.log(str1);
+
+  let str2 = t.split('').sort().join('');
+
+  console.log(str2);
+  if(str1 === str2){
+    return true;
+  } else {
+    return false;
+  }
+
+
 }
 
 function reverseBaseSort(arr) {
@@ -29,7 +49,7 @@ function reverseBaseSort(arr) {
 
 
 function frequencySort(arr) {
-  
+
 }
 
 module.exports = [
@@ -39,4 +59,3 @@ module.exports = [
   frequencySort,
   ageSort,
 ];
-
